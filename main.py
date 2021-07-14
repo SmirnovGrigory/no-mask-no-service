9108207a73c484eb52120425fc1dd871e3cfc0c9
Смирнов Григорий <greg.smirnov2013@yandex.ru>
2021-07-14 16:19:49 +0300

    add image_capture_processing

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import cv2
 import sys
 import argparse
 import logging as log
+import time
 from openvino.inference_engine import IENetwork, IECore
 from postprocessing import post_processing
 from ieclassifier import InferenceEngineClassifier
+
+sys.path.append(
+    'C:\\Program Files (x86)\\Intel\openvino_2021.4.582\\deployment_tools\\open_model_zoo\\demos\\common\python')
+from images_capture import open_images_capture
 
 
 def build_argparser():
@@ -28,34 +33,49 @@ def build_argparser():
     return parser
 
 
-def main():
-    log.basicConfig(format="[ %(levelname)s ] %(message)s",
-                    level=log.INFO, stream=sys.stdout)
-    args = build_argparser().parse_args()
-
-    #log.info("Start IE classification sample")
-
-    log.basicConfig(format="[ %(levelname)s ] %(message)s",
-                    level=log.INFO, stream=sys.stdout)
-    args = build_argparser().parse_args()
-    log.info("Start IE classification sample")
-    ie_classifier = InferenceEngineClassifier(configPath=args.model,
-                                              weightsPath=args.weights,
-                                              device=args.device,
-                                              extension=args.cpu_extension,
-                                              classesPath=args.classes)
-    image = cv2.imread(args.input)
+def single_image_processing(ie_classifier, input_img):
+    image = cv2.imread(input_img)
     y_bboxes_output, y_cls_output = ie_classifier.classify(image)
-    #log.info(y_bboxes_output)
+
     image = post_processing(image, y_bboxes_output, y_cls_output)
 
     cv2.imshow("result", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    cv2.imwrite('result_img1.jpg', image)
-    # predictions = ie_classifier.get_top(prob, 5)
-    # log.info("Predictions: " + str(predictions))
+    cv2.imwrite('results\\result_img' + str(round(time.time())) + '.jpg', image)
 
+
+def image_capture_processing(ie_classifier, input_cap):
+    cap = open_images_capture(input_cap, True)
+    output = cv2.VideoWriter('output_camera.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 20, (1280, 720))
+    while True:
+        image = cap.read()
+
+        y_bboxes_output, y_cls_output = ie_classifier.classify(image)
+        image = post_processing(image, y_bboxes_output, y_cls_output)
+
+        cv2.imshow("result", image)
+        output.write(image)
+
+        # Wait 1 ms and check pressed button to break the loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+
+def main():
+    log.basicConfig(format="[ %(levelname)s ] %(message)s",
+                    level=log.INFO, stream=sys.stdout)
+
+    args = build_argparser().parse_args()
+
+    ie_classifier = InferenceEngineClassifier(configPath=args.model,
+                                              weightsPath=args.weights,
+                                              device=args.device,
+                                              extension=args.cpu_extension,
+                                              classesPath=args.classes)
+
+    #single_image_processing(ie_classifier, args.input)
+    image_capture_processing(ie_classifier, args.input)
     return
 
 
